f5912c957fb76895347a10067bb9fe88bc105691
davidmrtn07 <davidmrtn07@gmail.com>
2024-03-07 12:13:28 -0600

    Fifth Try

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, Everyone!");
-        }
-    }
-}
+﻿// Declaring and Initializing Variables
+string name = "Rob";
+int age = 32;
+char middleInitial = 'D';
+bool isOver18 =true;
+double currentTemp = 70.9;
+decimal currentPrice = 45.89m;
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
